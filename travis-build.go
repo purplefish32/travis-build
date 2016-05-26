30c3182f41a9acd7f1f605c6e9d85e01ecb9cbf1
Donovan Tengblad <purplefish32@gmail.com>
2016-05-26 13:51:01 +0200

    Fixed double deployment bug

--- travis-build.go
+++ travis-build.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"strings"
+	"syscall"
 
 	"github.com/spf13/viper"
 	"github.com/sromku/go-gitter"
@@ -26,6 +28,22 @@ func clarobotSay(phrase string) {
 	}
 }
 
+func printCommand(cmd *exec.Cmd) {
+	fmt.Printf("==> Executing: %s\n", strings.Join(cmd.Args, " "))
+}
+
+func printError(err error) {
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("==> Error: %s\n", err.Error()))
+	}
+}
+
+func printOutput(outs []byte) {
+	if len(outs) > 0 {
+		fmt.Printf("==> Output: %s\n", string(outs))
+	}
+}
+
 func main() {
 	viper.SetConfigName("travis-build")       // name of config file (without extension)
 	viper.AddConfigPath("/etc/travis-build/") // path to look for the config file in
@@ -39,7 +57,7 @@ func main() {
 	room = viper.GetString("Room")
 
 	app := cli.NewApp()
-	app.Version = "0.0.3"
+	app.Version = "0.0.4"
 	app.Name = "travis-build"
 	app.Usage = "Manage Claroline Connect Travis Build deployments"
 	app.Commands = []cli.Command{
@@ -48,22 +66,39 @@ func main() {
 			Aliases: []string{"c"},
 			Usage:   "Deploy a new Claroline Connect Travis build",
 			Action: func(c *cli.Context) error {
-				resp, err := http.Get("http://" + travisFilesUrlPrefix + c.Args().First() + travisFilesUrlPostfix)
-				if (err == nil) && (resp.StatusCode == 200) {
-					fmt.Println("Deploying: ", c.Args().First())
-					clarobotSay("I am deploying a travis build here: [" + c.Args().First() + "](http://" + c.Args().First() + "." + travisBuildUrl + "), the build will be up and running in about 30s")
+				cst := "docker ps --format {{.Names}} | grep " + c.Args().First()
+				cmd := exec.Command("bash", "-c", cst)
+				var waitStatus syscall.WaitStatus
+				if err := cmd.Run(); err != nil {
+					// Did the command fail because of an unsuccessful exit code
+					if exitError, ok := err.(*exec.ExitError); ok {
+						resp, err := http.Get("http://" + travisFilesUrlPrefix + c.Args().First() + travisFilesUrlPostfix)
+						if (err == nil) && (resp.StatusCode == 200) {
+							fmt.Println("Deploying: ", c.Args().First())
+							clarobotSay("I am deploying a travis build here: [" + c.Args().First() + "](http://" + c.Args().First() + "." + travisBuildUrl + "), the build will be up and running in about 30s")
+						} else {
+							clarobotSay("Sorry dudes and dudettes I cant reach the travis build (" + c.Args().First() + "), it probably does not exist!")
+							os.Exit(1)
+						}
+						cst = "docker run -id -e BUILD=" + c.Args().First() + " -e VIRTUAL_HOST=" + c.Args().First() + "." + travisBuildUrl + " -p 80 --name " + c.Args().First() + " -t claroline"
+						if err := exec.Command("bash", "-c", cst).Run(); err != nil {
+							fmt.Fprintln(os.Stderr, err)
+							clarobotSay("Sorry somthing went wrong deploying the following build: " + c.Args().First())
+							os.Exit(1)
+						}
+						fmt.Println("Successfully deployed the following Travis build: " + c.Args().First())
+
+						waitStatus = exitError.Sys().(syscall.WaitStatus)
+						printOutput([]byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+					}
 				} else {
-					clarobotSay("Sorry dudes and dudettes I cant reach the travis build (" + c.Args().First() + "), it probably does not exist!")
+					fmt.Println("The Travis build " + c.Args().First() + " is allready deployed")
+					clarobotSay("The Travis build " + c.Args().First() + " is allready deployed")
 					os.Exit(1)
+					waitStatus = cmd.ProcessState.Sys().(syscall.WaitStatus)
+					printOutput([]byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
 				}
-				cmd := "docker"
-				args := []string{"run", "-id", "-e", "BUILD=" + c.Args().First(), "-e", "VIRTUAL_HOST=" + c.Args().First() + "." + travisBuildUrl, "-p", "80", "--name", c.Args().First(), "-t", "claroline"}
-				if err := exec.Command(cmd, args...).Run(); err != nil {
-					fmt.Fprintln(os.Stderr, err)
-					clarobotSay("Sorry somthing went wrong deploying the following build: " + c.Args().First())
-					os.Exit(1)
-				}
-				fmt.Println("Successfully deployed the following Travis build: " + c.Args().First())
+
 				return nil
 			},
 		},
